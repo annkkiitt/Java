38857c545d61f72fcf55362c77bdaa7646bb900e
Ankit Rawat <ankit2001rawat@gmail.com>
2022-03-27 18:00:26 +0530

    First Java Program

---
+++ Fundamentals_Of_Java/Unit_01/Practical_01/P1_Task01_FirstProgramInJava.java
@@ -0,0 +1,11 @@
+package Fundamentals_Of_Java.Unit_01.Practical_01;
+
+/**
+ * P1_Task01_FirstProgramInJava
+ */
+public class P1_Task01_FirstProgramInJava {
+
+    public static void main(String[] args) {
+        System.out.println("First Program in java");
+    }
+}
